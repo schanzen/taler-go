1f18b2c68ce49ed67d318008a9d7cb00a7cd9410
Martin Schanzenbach <schanzen@gnunet.org>
2025-04-26 09:25:32 +0200

    update tos API

--- pkg/rest/tos.go
+++ pkg/rest/tos.go
@@ -6,17 +6,26 @@ import (
 	"mime"
 	"net/http"
 	"os"
-	"strings"
 
 	"golang.org/x/text/language"
-	"gopkg.in/ini.v1"
 )
 
-func ServiceTermsResponse(s *ini.Section, w http.ResponseWriter, r *http.Request) {
-	fileType := s.Key("default_doc_filetype").MustString("text/html")
-	termsLocation := s.Key("default_tos_path").MustString("terms/")
+type TalerTosConfig struct {
+  // The default file type
+  DefaultFileType string
+
+  // Supported file types
+  SupportedFileTypes []string
+
+  // Default language
+  DefaultLanguage string
+}
+
+func ServiceTermsResponse(w http.ResponseWriter, r *http.Request, termsdatahome string, cfg TalerTosConfig) {
+	fileType := cfg.DefaultFileType
+	termsLocation := termsdatahome
 	for _, typ := range r.Header["Accept"] {
-		for _, a := range strings.Split(s.Key("supported_doc_filetypes").String(), " ") {
+		for _, a := range cfg.SupportedFileTypes {
 			if typ == a {
 				fileType = a
 			}
@@ -40,7 +49,7 @@ func ServiceTermsResponse(s *ini.Section, w http.ResponseWriter, r *http.Request
 		}
 	}
 	// Default document in expected/default format
-	defaultLanguage := s.Key("default_doc_lang").MustString("en")
+	defaultLanguage := cfg.DefaultLanguage
 	extensions, _ := mime.ExtensionsByType(fileType)
 	for _, ext := range extensions {
 		docFile := fmt.Sprintf("%s/%s/0%s", termsLocation, defaultLanguage, ext)
@@ -55,44 +64,45 @@ func ServiceTermsResponse(s *ini.Section, w http.ResponseWriter, r *http.Request
 	w.WriteHeader(http.StatusNotFound)
 }
 
-func PrivacyPolicyResponse(s *ini.Section, w http.ResponseWriter, r *http.Request) {
-	fileType := s.Key("default_doc_filetype").MustString("text/html")
-	termsLocation := s.Key("default_pp_path").MustString("privacy/")
-	for _, typ := range r.Header["Accept"] {
-		for _, a := range strings.Split(s.Key("supported_doc_filetypes").String(), " ") {
-			if typ == a {
-				fileType = a
-			}
-		}
-	}
 
-	if len(r.Header.Get("Accept-Language")) != 0 {
-		acceptLangs, _, _ := language.ParseAcceptLanguage(r.Header.Get("Accept-Language"))
-		for _, lang := range acceptLangs {
-			extensions, _ := mime.ExtensionsByType(fileType)
-			for _, ext := range extensions {
-				docFile := fmt.Sprintf("%s/%s/0%s", termsLocation, lang.String(), ext)
-				log.Printf("Trying %s\n", docFile)
-				fileBytes, err := os.ReadFile(docFile)
-				if nil == err {
-					w.Header().Set("Content-Type", fileType)
-					w.Write(fileBytes)
-					return
-				}
-			}
-		}
-	}
-	// Default document in expected/default format
-	defaultLanguage := s.Key("default_doc_lang").MustString("en")
-	extensions, _ := mime.ExtensionsByType(fileType)
-	for _, ext := range extensions {
-		docFile := fmt.Sprintf("%s/%s/0%s", termsLocation, defaultLanguage, ext)
-		fileBytes, err := os.ReadFile(docFile)
-		if nil == err {
-			w.Header().Set("Content-Type", fileType)
-			w.Write(fileBytes)
-			return
-		}
-	}
-	w.WriteHeader(http.StatusNotFound)
+func PrivacyPolicyResponse(w http.ResponseWriter, r *http.Request, policydatahome string, cfg TalerTosConfig) {
+	fileType := cfg.DefaultFileType
+	termsLocation := policydatahome
+  for _, typ := range r.Header["Accept"] {
+    for _, a := range cfg.SupportedFileTypes {
+      if typ == a {
+        fileType = a
+      }
+    }
+  }
+
+  if len(r.Header.Get("Accept-Language")) != 0 {
+    acceptLangs, _, _ := language.ParseAcceptLanguage(r.Header.Get("Accept-Language"))
+    for _, lang := range acceptLangs {
+      extensions, _ := mime.ExtensionsByType(fileType)
+      for _, ext := range extensions {
+        docFile := fmt.Sprintf("%s/%s/0%s", termsLocation, lang.String(), ext)
+        log.Printf("Trying %s\n", docFile)
+        fileBytes, err := os.ReadFile(docFile)
+        if nil == err {
+          w.Header().Set("Content-Type", fileType)
+          w.Write(fileBytes)
+          return
+        }
+      }
+    }
+  }
+  // Default document in expected/default format
+  defaultLanguage := cfg.DefaultLanguage
+  extensions, _ := mime.ExtensionsByType(fileType)
+  for _, ext := range extensions {
+    docFile := fmt.Sprintf("%s/%s/0%s", termsLocation, defaultLanguage, ext)
+    fileBytes, err := os.ReadFile(docFile)
+    if nil == err {
+      w.Header().Set("Content-Type", fileType)
+      w.Write(fileBytes)
+      return
+    }
+  }
+  w.WriteHeader(http.StatusNotFound)
 }
